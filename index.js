8e7b9e165159180802b3e623ae3d6226735dd321
Ado555 <116297586+Ado555@users.noreply.github.com>
2023-02-14 11:04:30 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// oppgave 1
+//let i = 0;
+//while (i < 50) {
+//    console.log(i);
+//    i++;
+//}
+// oppgave2
+//let i = 2;
+//while (i <= 100) {
+//  console.log(i);
+//  i += 2;
+//}
+// oppgave3
+//let i = 0;
+//while (i <= 999) {
+//  console.log(i);
+//  i += 3;
+//}
+// opgave4
+//let i = 1;
+//while (i <= 1000) {
+//  if (i % 3 === 0 && i % 5 === 0) {
+//    console.log(i);
+//  }
+//  i++;
+//}
+//oppgave5
+//let sum = 0;
+//let i = 1;
+//
+//while (i <= 100) {
+//    sum += i;
+//    i++;
+//}
+//console.log(sum);
+//oppgave6
+
+//let i = 1;
+//while (i <= 4) {
+//  console.log("#".repeat(i));
+//  i++;
+//}
+//oppgave7
+
+//let i = 1;
+//while (i <= 50) {
+//  console.log("#".repeat(i))
+//  ;i++;
+//}
+//oppgave8
+//let i = 1; while (i <= 100) {
+// if (i % 3 === 0 && i % 5 === 0) {
+// console.log("FizzBuzz");
+// } else if (i % 3 === 0) {
+// console.log("Fizz");
+// } else if (i % 5 === 0) {
+// console.log("Buzz");
+// } else {
+// console.log(i);
+// }
+// i++;
+//    }
+
